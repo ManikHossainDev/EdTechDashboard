3138ac22be25c0cc5f645a76b7ef9cbf2ba0ff95
FahimJadid <b170606023@pharm.jnu.ac.bd>
2026-02-07 12:37:04 +0600

    env reversed

--- src/redux/baseApi/baseApi.js
+++ src/redux/baseApi/baseApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "pokemonApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.BACKEND_URL,
+    baseUrl: import.meta.env.VITE_BACKEND_URL,
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
